e755a15f222fc1132e5ec9ed51ca5d81482d5666
Ahliddin Akbaraliyev <akbaraliyevahliddin241@gmail.com>
2025-10-09 10:10:35 +0500

    add author

--- handler/user.py
+++ handler/user.py
@@ -107,7 +107,7 @@ async def get_checked_books(call: CallbackQuery):
 
         await call.message.answer_photo(
             photo=FSInputFile(path=book_path),
-            caption=f"{book[1]}\n\n{book[2]}",
+            caption=f"{book[1]}\n\n{book[2]}\n\n{book[3]}",
             reply_markup=plus_minus_inline_button(book_id=book[0], count=1)
         )
     
