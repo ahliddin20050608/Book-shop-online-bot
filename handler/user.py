fa0d4958896d7a1d1a811c30c810cdb76455b527
Ahliddin Akbaraliyev <akbaraliyevahliddin241@gmail.com>
2025-10-10 11:09:32 +0500

    ajjdisj

--- handler/user.py
+++ handler/user.py
@@ -121,7 +121,7 @@ async def get_checked_books(call: CallbackQuery):
 @user_router.callback_query(F.data.startswith("minus"))
 async def minus_button(call: CallbackQuery):
      for i in CHECKED_BOOKS:
-        book = find_books(i[1], i[2])
+        book = find_by_books_id(i)
         if int(book[0]) == int(call.data.split("_")[1]):
             book_id = int(call.data.split("_")[-1])
             count = int(call.data.split("_")[1])
@@ -137,7 +137,7 @@ async def minus_button(call: CallbackQuery):
 @user_router.callback_query(F.data.startswith("plus"))
 async def plus_button(call: CallbackQuery):
     for i in CHECKED_BOOKS:
-        book = find_books(i[1],i[2])
+        book = find_by_books_id(i)
         if int(book[0]) == int(call.data.split("_")[1]):
 
             book_id = int(call.data.split("_")[-1])
@@ -154,7 +154,7 @@ async def plus_button(call: CallbackQuery):
 @user_router.callback_query(F.data.startswith("save_"))
 async def save_book_by_id(call:CallbackQuery):
      for i in CHECKED_BOOKS:
-        book = find_books(i[1],i[2])
+        book = find_by_books_id(i)
         if int(book[0]) == call.data.split("_")[1]:
             count = call.data.split("_")[1]
             book_id = int(call.data.split("_")[-1])
